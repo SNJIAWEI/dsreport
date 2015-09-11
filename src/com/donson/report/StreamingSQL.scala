ae5a99aa9554ed24e584ee5e033b33193c468cc1
Jiawei <64341393@qq.com>
2015-09-11 15:46:31 +0800

    添加测试参数

--- src/com/donson/report/StreamingSQL.scala
+++ src/com/donson/report/StreamingSQL.scala
@@ -12,23 +12,27 @@ object StreamingSQL {
 
   def main(args: Array[String]) {
     LoggerLevel.setStreamingLogLevels(Level.WARN)
-    if (args.length != 4) {
-      System.err.println("Usage: StreamingSQL <dbUrl> <tableName> <user> <password> <filePath>")
-      System.exit(1);
-    }
+//    if (args.length != 4) {
+//      System.err.println("Usage: StreamingSQL <dbUrl> <tableName> <user> <password> <filePath>")
+//      System.exit(1);
+//    }
 
-    //    val Array(dbUrl, dbUser, dbPassword, tableName) = args
-    //
+    //    val Array(zkQuorum, group, topics, numThreads, outurl) = args
+    val zkQuorum = "192.168.1.221:2181,192.168.1.222:2181,192.168.1.223:2181"
+    val group = "test-group"
+    val topics = "testTopic"
+    val numThreads = 1
+
     // 配置数据库连接参数
     val connectionProperties = new Properties()
     connectionProperties.setProperty("user", "root")
-    connectionProperties.setProperty("password", "123")
+    connectionProperties.setProperty("password", "123456")
     connectionProperties.setProperty("driver", "com.mysql.jdbc.Driver")
 
-    val Array(zkQuorum, group, topics, numThreads, outurl) = args
-    val sparkConf = new SparkConf().setAppName("StreamingSQL").setMaster ("local[2]")
-    val ssc = new StreamingContext(sparkConf, Seconds(30)) /* duringtime  min */
+    val sparkConf = new SparkConf().setAppName("StreamingSQL").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(120))
     /*ssc.checkpoint("hdfs://master:9000/ck6")*/
+
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
 
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
@@ -38,17 +42,22 @@ object StreamingSQL {
 
       val rowRDD = Utils.getRowRDD(rdd)
       val rddDataFrame = sqlContext.createDataFrame(rowRDD, schema)
-      rddDataFrame.write.parquet("hdfs://192.168.1.220:9000/donson/streaming/people" + Utils.formateFileName + ".parquet")
+      rddDataFrame.write.parquet("hdfs://192.168.1.220:9000/donson/streaming/report1" + Utils.formateFileName + ".parquet")
 
-      val parquetFile = sqlContext.read.parquet("hdfs://192.168.1.220:9000/donson/streaming/people" + Utils.formateFileName + ".parquet")
+      val parquetFile = sqlContext.read.parquet("hdfs://192.168.1.220:9000/donson/streaming/report1" + Utils.formateFileName + ".parquet")
       parquetFile.registerTempTable("adloginfo")
+      // cache table
+//      sqlContext.cacheTable("adloginfo")
+
 
       // 查询结果并存储到数据库中
       val results = sqlContext.sql("SELECT AdvertisersID,ADOrderID,ADCreativeID,ReqDate,ReqHour,sum(IsShow),sum(IsClick),sum(IsTakeBid),sum(IsSuccessBid) " +
         "FROM adloginfo " +
         "group by AdvertisersID,ADOrderID,ADCreativeID,ReqDate,ReqHour")
       // results.collect().foreach(println)
-      results.write.mode(SaveMode.Append).jdbc("jdbc:mysql://192.168.1.129:3306/test", "donson1", connectionProperties)
+      results.write.mode(SaveMode.Append).jdbc("jdbc:mysql://192.168.1.130:3306/test", "report_1", connectionProperties)
+      // uncache table
+//      sqlContext.uncacheTable("adloginfo")
     }
 
     ssc.start()
